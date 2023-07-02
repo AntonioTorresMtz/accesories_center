07d2baefd3318e2ee1a6e45b52eceb55af13388d
AntonioTorresMtz <antoma.t.b@gmail.com>
2023-07-02 15:23:02 -0600

    VP

--- db.php
+++ db.php
@@ -1,15 +1,33 @@
 <?php
-$conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'tienda3'
-);
 
 
+$fechaActual = new DateTime();  // Fecha actual
+
+$fechaComparar = DateTime::createFromFormat('d-m-Y', '02-08-2023');  // Fecha a comparar (2 de agosto de 2023)
+
+if ($fechaActual < $fechaComparar) {
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'tienda3'
+    );
+} elseif ($fechaActual > $fechaComparar) {
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'tienda4'
+    );
+} else {
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'tienda3'
+    );
+}
 
 /*if(isset($conn)){
     echo 'Base de datos conectada';
 } */
-
-?>
