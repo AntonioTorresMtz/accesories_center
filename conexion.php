a0466fb341387a556b56aecc34e0faf215175d40
= <=>
2023-07-03 17:30:35 -0600

    Fecha

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php 
 function getConnexion()
 {
-  $mysqli = new Mysqli('localhost', 'root', '', 'tienda3');
+  $mysqli = new Mysqli('localhost', 'root', '', 'tienda4');
   if($mysqli->connect_errno) exit('Error en la conexión: ' . $mysqli->connect_errno);
   $mysqli->set_charset('utf8');
   return $mysqli;
--- db.php
+++ db.php
@@ -3,28 +3,28 @@
 
 $fechaActual = new DateTime();  // Fecha actual
 
-$fechaComparar = DateTime::createFromFormat('d-m-Y', '02-08-2023');  // Fecha a comparar (2 de agosto de 2023)
+$fechaComparar = DateTime::createFromFormat('d-m-Y', '03-08-2023');  // Fecha a comparar (3 de agosto de 2023)
 
 if ($fechaActual < $fechaComparar) {
     $conn = mysqli_connect(
         'localhost',
         'root',
         '',
-        'tienda3'
+        'tienda4'
     );
 } elseif ($fechaActual > $fechaComparar) {
     $conn = mysqli_connect(
         'localhost',
         'root',
         '',
-        'tienda4'
+        'tienda5'
     );
 } else {
     $conn = mysqli_connect(
         'localhost',
         'root',
         '',
-        'tienda3'
+        'tienda4'
     );
 }
 
