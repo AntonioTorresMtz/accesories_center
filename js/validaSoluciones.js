845091de3d3cececda7f3344da755555f79e8f7d
= <=>
2024-10-31 17:16:04 -0600

    Se agrego validacion para que solo se envie el form con IMEIs donde haya coincidencias

--- js/validaSoluciones.js
+++ js/validaSoluciones.js
@@ -1,4 +1,5 @@
 formulario = document.getElementById("formulario");
+var resultado_imei = false;
 
 $(document).ready(function () {
   $("#imei").focus();
@@ -8,13 +9,27 @@ $(document).ready(function () {
       type: "POST",
       url: "buscar/telefonoCambio.php",
       data: { busca: busca },
+      dataType: "json", // Especifica que la respuesta es JSON
     })
-
-      .done(function (resultado) {
-        $("#result").html(resultado);
+      .done(function (respuesta) {
+        // Limpia el contenedor antes de mostrar el resultado
+        $("#result").empty();
+        resultado_imei = respuesta.resultado;
+        console.log("Respuesta del server: " + respuesta.resultado);
+        if (respuesta.resultado) {
+          // Itera sobre los resultados y muestra cada uno
+          respuesta.data.forEach(function (telefono) {
+            $("#result").append(
+              "<p><b>Teléfono:</b> " + telefono.modelo + "</p>"
+            );
+          });
+        } else {
+          // Muestra el mensaje de error si no se encontraron resultados
+          $("#result").append("<p> " + respuesta.mensaje + "</p>");
+        }
       })
       .fail(function () {
-        alert("Ocurrio un error");
+        alert("Ocurrió un error");
       });
   });
 });
@@ -27,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function validarSolucion(evento) {
   evento.preventDefault();
+  console.log(resultado_imei);
   var solucion = document.getElementById("solucion").value;
   if (solucion == 0) {
     type =
@@ -38,6 +54,17 @@ function validarSolucion(evento) {
       );
     return;
   } else {
-    this.submit();
+    if (resultado_imei) {
+      this.submit();
+      console.log("Aqui se enviaria el formulario");
+    } else {
+      type =
+        "text/javascript" >
+        Swal.fire(
+          "IMEI no encontrado!",
+          "No se encontro un celular para el cambio con el IMEI ingresado, verificalo",
+          "error"
+        );
+    }
   }
 }
